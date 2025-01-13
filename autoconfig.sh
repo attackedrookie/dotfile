7a03cf4e63f9826ba2c74770b212a9b1cd863dc4
100013112-Lin Jian Nan <18350239892@163.com>
2025-01-13 22:06:04 +0800

    修改自动配置文件，添加更便捷的链接方式

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,8 +1,13 @@
  #!/bin/bash
- files=$(ls -a $1 | grep -E '.[^.]+' |grep -v .git)
+ 
+SCRIPT_FILE=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_FILE")
+
+ files=$(ls -a $SCRIPT_DIR | grep -E '.[^.]+' |grep -v .git)
  # 去掉 ls -a 返回结果中的 ". .. .git"
  for file in `echo $files`; do
-     ln -s $1/$file ~/$file # 创建软链接
+     rm -f ~/$file
+     ln -s $SCRIPT_DIR/$file ~/$file # 创建软链接
  done
  
  # .tmux.conf 使用tmux source-file运行
